d0233777a5520029e351ec817beeac83e143db2e
YosCal777 <143665213+YosCal777@users.noreply.github.com>
2023-09-30 20:07:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-import express from "express";
-import cors from 'cors';
-import bodyParser from 'body-parser';
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 const getVarianteManual = async (type, num1, num2) => {
     try {
         // RESULT VC
