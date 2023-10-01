a84378c98a950756e7f05daab7d4c280d6edffdc
YosCal777 <143665213+YosCal777@users.noreply.github.com>
2023-09-30 21:56:50 -0400

    agregamos express

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import express from "express";
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const getVarianteManual = async (type, num1, num2) => {
     try {
         // RESULT VC
